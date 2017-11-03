ccbad8f5e079813de81118dc0fb14a782385e01f
kengkungkeng <kengkungkeng@gmail.com>
2017-11-03 18:43:31 +0700

    Full search complete

---
+++ app/src/main/java/com/example/user/bustogoappliction/Search/ListEnty.java
@@ -0,0 +1,17 @@
+package com.example.user.bustogoappliction.Search;
+
+/**
+ * Created by Keng on 3/11/2560.
+ */
+
+public class ListEnty {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
